d7cf77711dea07ad5fa8307a2b1d3cded6106e64
ZaidAli-19 <zaidddali19@gmail.com>
2025-10-01 16:45:18 +0530

    failed

--- src/main/java/com/example/demo_java/util/Message.java
+++ src/main/java/com/example/demo_java/util/Message.java
@@ -4,8 +4,5 @@ package com.example.demo_java.util;
  * A simple message class.
  */
 public class Message {
-    /**
-     * String message.
-     */
-    private final String message = "Helloodsfhbsidhfbisdhbfidhbsfhkbghbfhglbdzshfbkdzbfgbfksbfudsbfuybsdfksbdfjksbdf!";
+    private final String message = "Hello!";
 }
