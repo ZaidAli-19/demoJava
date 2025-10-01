5d26a87d8ed58dd720c1f116da42e395af195d4e
ZaidAli-19 <zaidddali19@gmail.com>
2025-10-01 11:51:34 +0530

    Fixed error

--- src/main/java/com/example/demo_java/DemoJavaApplication.java
+++ src/main/java/com/example/demo_java/DemoJavaApplication.java
@@ -11,7 +11,7 @@ public class DemoJavaApplication {
 
 
 /** The name of the application. */
-private final String name = "DemoJavaApplicationfdgszdgdsfszdfsdfsdfsdzgfsgsfdgdsgfsdgsdf";
+private final String name = "DemoJavaApplication";
 /**
 * The entry point of the application.
 * @param args Command-line arguments.
