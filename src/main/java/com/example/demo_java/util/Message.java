dc634c8c63ac9919bbd8b86965b46bedd9ee2300
ZaidAli-19 <zaidddali19@gmail.com>
2025-10-01 12:15:17 +0530

    fix code

--- src/main/java/com/example/demo_java/util/Message.java
+++ src/main/java/com/example/demo_java/util/Message.java
@@ -4,5 +4,8 @@ package com.example.demo_java.util;
  * A simple message class.
  */
 public class Message {
+    /**
+     * String message.
+     */
     private final String message = "Hello!";
 }
